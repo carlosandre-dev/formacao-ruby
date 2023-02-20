70c34501c11e5a60c4068a1a93ca3ebeebd440da
carlosandre-dev <andre.tjlba@gmail.com>
2023-02-20 12:37:11 -0300

    Condicionais if, elsif e else

---
+++ estruturas-controle/if_elsif_else.rb
@@ -0,0 +1,18 @@
+print "Informe o dia da semana de 1 a 7: "
+dia = gets.chomp
+
+if dia == '1'
+    puts ("Domingo")
+elsif dia == '2'
+    puts ("Segunda")
+elsif dia == '3'
+    puts ("Terça")
+elsif dia == '4'
+    puts ("Quarta")
+elsif dia == '5'
+    puts ("Quinta")
+elsif dia == '6'
+    puts ("Sexta")
+elsif dia == '7'
+    puts ("Sábado")
+end
